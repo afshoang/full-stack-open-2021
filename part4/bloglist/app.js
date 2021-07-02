1ada5a15aaff303e7c618e0bc8685dfd24e72ebf
afshoang <kaitokid19xx@gmail.com>
2021-07-02 13:57:02 +0700

    4.2 refactor

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,44 +1,28 @@
-require('dotenv').config()
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
 
 mongoose
-  .connect(process.env.MONGODB_URI, {
+  .connect(config.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
     useCreateIndex: true,
   })
   .then(() => {
-    console.log('connected to MongoDB')
+    logger.info('connected to MongoDB')
   })
   .catch((error) => {
-    console.error('error connecting to MongoDB:', error.message)
+    logger.error('error connecting to MongoDB:', error.message)
   })
 
 app.use(express.json())
 app.use(cors())
 
-app.get('/', (req, res) => {
-  res.send('<h1>This is Blog list app2</h1>')
-})
-
-app.get('/api/blogs', (req, res) => {
-  Blog.find({}).then((blogs) => {
-    res.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (req, res) => {
-  console.log(req.body)
-  const blog = new Blog(req.body)
-
-  blog.save().then((result) => {
-    res.status(201).json(result)
-  })
-})
+app.use('/api/blogs', blogsRouter)
 
 module.exports = app
